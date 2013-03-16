d92d3870bf5aa1401d683635422ba998110ffc5a
J Wolfgang Goerlich <jwgoerlich@gmail.com>
2013-03-16 13:42:44 -0400

    Extended DerivedValue to return numbers

--- Library/SimWitty.Library.Core/Encrypting/DerivedValue.cs
+++ Library/SimWitty.Library.Core/Encrypting/DerivedValue.cs
@@ -53,22 +53,170 @@ namespace SimWitty.Library.Core.Encrypting
         /// <returns>A value derived from several rounds of hashing and truncating.</returns>
         public string GetString(uint rounds, int length)
         {
+            byte[] value = this.GetBytes(rounds, length);
+            string base64 = Convert.ToBase64String(value);
+
+            if (length > base64.Length) length = base64.Length;
+            return base64.Substring(0, length);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <param name="length">The character length of the resulting value.</param>
+        /// <returns>A value derived from several rounds of hashing and truncating.</returns>
+        public byte[] GetBytes(uint rounds, int length)
+        {
             System.Security.Cryptography.SHA256 sha = new System.Security.Cryptography.SHA256Managed();
-            string value = string.Empty;
             byte[] buffer = this.GetHash();
+
+            if (length < 1) length = 1;
+            if (length > buffer.Length) length = buffer.Length;
 
             for (uint u = 0; u < rounds; u++)
             {
                 byte[] hash = sha.ComputeHash(buffer);
-                string hashBase64 = System.Convert.ToBase64String(hash);
-                int end = Math.Min(length, hashBase64.Length);
-                value = hashBase64.Substring(0, end);
-                buffer = System.Text.Encoding.ASCII.GetBytes(value);
+                buffer = new byte[length];
+                Array.Copy(hash, buffer, length);
             }
 
             sha.Dispose();
-            return value;
+            return buffer;
         }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 16-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public ushort GetUInt16()
+        {
+            return this.GetUInt16(3);
+        }
+        
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 16-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public ushort GetUInt16(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 2);
+            return BitConverter.ToUInt16(value, 0);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 32-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public uint GetUInt32()
+        {
+            return this.GetUInt32(3);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 32-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public uint GetUInt32(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 4);
+            return BitConverter.ToUInt32(value, 0);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 64-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public ulong GetUInt64()
+        {
+            return this.GetUInt64(3);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 64-bit unsigned integer value derived from several rounds of hashing and truncating.</returns>
+        public ulong GetUInt64(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 8);
+            return BitConverter.ToUInt64(value, 0);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 16-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public short GetInt16()
+        {
+            return this.GetInt16(3);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 16-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public short GetInt16(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 2);
+            return BitConverter.ToInt16(value, 0);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 32-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public int GetInt32()
+        {
+            return this.GetInt32(3);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 32-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public int GetInt32(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 4);
+            return BitConverter.ToInt32(value, 0);
+        }
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <returns>A 64-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public long GetInt64()
+        {
+            return this.GetInt64(3);
+        }   
+
+        /// <summary>
+        /// Obtain a derived value from a pre-shared key using several rounds of hashing.
+        /// The truncated value has a high rate of collisions. However, with a high collision rate, the ability to reverse the value to a pre-shared key is lowered.
+        /// </summary>
+        /// <param name="rounds">The number of times to hash before returning the value.</param>
+        /// <returns>A 64-bit signed integer value derived from several rounds of hashing and truncating.</returns>
+        public long GetInt64(uint rounds)
+        {
+            byte[] value = this.GetBytes(rounds, 8);
+            return BitConverter.ToInt64(value, 0);
+        }        
 
         /// <summary>
         /// Get the hash value of the pre-shared key.
